ade11171f401f2ab63cefd3c1d04c3139ed23625
Christian Jánský <chrisjansky@gmail.com>
2016-08-11 14:55:41 -0700

    JS Created v-scrollto.js

---
+++ app/assets/js/v-scrollto.js
@@ -0,0 +1,8 @@
+// Scroll to anchor.
+$("[data-scrollto]").click(function(event) {
+  event.preventDefault();
+
+  $("html, body").animate({
+    scrollTop: $($(this).attr("href")).offset().top
+  }, 250);
+});
