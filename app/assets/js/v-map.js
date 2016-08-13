f43650b90305496d2d51c595104ba4378581dc01
Christian Jánský <chrisjansky@gmail.com>
2016-08-12 21:22:40 -0700

    JADE SCSS JS Changed Google Maps implementation to use their API library

---
+++ app/assets/js/v-map.js
@@ -0,0 +1,26 @@
+// Google Maps.
+function initMap() {
+  var mapIframe = document.getElementById("js-map");
+
+  if (mapIframe !== null) {
+    var mapOptions = {
+      center: {lat: 49.4722996, lng: 17.1002256},
+      zoom: 15,
+      scrollwheel: false
+    }
+
+    var 
+      map = new google.maps.Map(mapIframe, mapOptions),
+      infoWindow = new google.maps.InfoWindow(),
+      mapMarker = new google.maps.Marker({
+        position: mapOptions.center,
+        map: map,
+        animation: google.maps.Animation.DROP
+      });
+    google.maps.event.addListener(mapMarker, "click", function() {
+      infoWindow.setContent("<div class='o-map__info'><h1>veneo</h1><p>Plumlovská 830/49<br>796 01, Prostějov</p></div>");
+      infoWindow.open(map, mapMarker);
+    });
+  }
+
+}
