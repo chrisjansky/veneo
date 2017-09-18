fb3a374e03d6a9c71af4b62f305fce7731f3d771
Christian Jánský <chrisjansky@gmail.com>
2017-09-18 13:40:04 +0200

    Changed o-filters to <ul> instead of <select>

---
+++ app/assets/js/v-dropdown.js
@@ -0,0 +1,12 @@
+$("[data-dropdown]").on("click", function(event) {
+  var $parent = $(this).closest("[data-parent]");
+
+  event.stopPropagation();
+  
+  $("[data-parent]").not($parent).removeClass("dropdown--is-active");
+  $parent.toggleClass("dropdown--is-active");
+});
+
+$(document).click(function() {
+  $("[data-parent]").removeClass("dropdown--is-active");
+})
