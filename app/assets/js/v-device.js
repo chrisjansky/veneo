e820f4a13c438fe616686aa7bd761abc25e55aa2
Christian Jánský <chrisjansky@gmail.com>
2017-09-20 16:02:00 +0200

    JS Fixed ordering, removed $ variables from v-device.js

--- app/assets/js/v-device.js
+++ app/assets/js/v-device.js
@@ -3,9 +3,9 @@ var currentDevice;
 if (isDesktop()) {
   currentDevice = "desktop";
 
-  $body.addClass("device--is-desktop");
+  $("body").addClass("device--is-desktop");
 } else {
   currentDevice = "mobile";
 
-  $body.addClass("device--is-mobile");
+  $("body").addClass("device--is-mobile");
 }
