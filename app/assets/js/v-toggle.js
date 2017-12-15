a9ffbc617f58c9bbcfca678df1550611da008245
Christian Jánský <chrisjansky@gmail.com>
2017-12-15 13:18:16 +0100

    Changed #modal opening interactions

--- app/assets/js/v-toggle.js
+++ app/assets/js/v-toggle.js
@@ -10,5 +10,23 @@ function veneoToggle() {
 
     $thisEl
       .toggleClass("toggle--is-active");
-  }); 
+  });
+
+  $("[data-remove]").on("click", function(event) {
+    event.preventDefault();
+
+    var
+      $thisEl = $(this);
+
+    $html
+      .removeClass($thisEl.attr("data-remove"));
+  });
+
+  var checkObj = document.querySelectorAll("[data-check]");
+
+  for (var i = 0; i < checkObj.length; i++) {
+    checkObj[i].addEventListener("click", function(event) {
+      $html.toggleClass(this.dataset.check, this.checked);
+    });
+  }
 }
